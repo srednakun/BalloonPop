6042b2f7b0cf97c0225552b9d12669fd12153830
Aaron <ajtaggs@gmail.com>
2019-08-17 20:56:00 -0500

    More renamig
    Missed a few of the references to 'example'

--- app/src/androidTest/java/com/sredna/balloonpop/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sredna/balloonpop/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.balloonpop", appContext.packageName)
+        assertEquals("com.sredna.balloonpop", appContext.packageName)
     }
 }
