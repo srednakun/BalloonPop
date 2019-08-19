5c9eb1f1647af3e1aa0b569cf30ba07123beec5e
Aaron <ajtaggs@gmail.com>
2019-08-18 22:06:00 -0500

    Created Balloon Game Activity
    Balloon floats to the top centered horizontally

---
+++ app/src/main/java/com/sredna/balloonpop/BalloonPopActivity.kt
@@ -0,0 +1,47 @@
+package com.sredna.balloonpop
+
+import android.animation.ObjectAnimator
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_balloonpop_game.*
+
+
+class BalloonPopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balloonpop_game)
+        createBalloon()
+    }
+
+    private fun createBalloon() {
+        val balloon: ImageView = LayoutInflater.from(this).inflate(R.layout.balloon_image_view, layoutBalloonpopGame, false) as ImageView
+        // Make the balloon invisible until it is placed where it is supposed to be to avoid flicker
+        balloon.visibility = View.INVISIBLE
+        layoutBalloonpopGame.addView(balloon)
+
+        // Post so that it waits to render the balloon when the width and height will be available
+        layoutBalloonpopGame.post {
+            // Move the balloon the the middle of the screen
+            val middleOfLayout = layoutBalloonpopGame.width / 2f
+            balloon.x = middleOfLayout - balloon.width / 2f
+            balloon.y = layoutBalloonpopGame.height.toFloat()
+
+            val startPosition = layoutBalloonpopGame.bottom.toFloat()
+            val endPosition = layoutBalloonpopGame.top.toFloat() - balloon.height
+            val yAnimator = ObjectAnimator.ofFloat(balloon, View.TRANSLATION_Y, startPosition, endPosition)
+
+            // How long it takes the balloon to move off the screen
+            yAnimator.duration = 2500L
+
+            // Show the balloon and start animating it
+            balloon.visibility = View.VISIBLE
+            yAnimator.start()
+        }
+    }
+}
